609780f561ff10a5141012b755394e64bf9ba775
Youssef Mohammed <youssefmohammed2093@gmail.com>
2024-09-17 16:52:00 +0300

    Workspace preferences

---
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -0,0 +1,148 @@
+import { toast } from "sonner";
+import { useState } from "react";
+import { TrashIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogHeader,
+  DialogTitle,
+  DialogClose,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { useConfirm } from "@/hooks/use-confirm";
+import { UseUpdateWorkspace } from "@/features/workspaces/api/use-update-workspace";
+import { UseRemoveWorkspace } from "@/features/workspaces/api/use-remove-workspace";
+
+interface PreferencesModalProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  initialValue: string;
+}
+
+export const PreferencesModal = ({
+  open,
+  setOpen,
+  initialValue,
+}: PreferencesModalProps) => {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+  const [value, setValue] = useState(initialValue);
+  const [editOpen, setEditOpen] = useState(false);
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "This action is irreversible."
+  );
+
+  const { mutate: updateWorkspace, isPending: isUpdatingWorkspace } =
+    UseUpdateWorkspace();
+
+  const { mutate: removeWorkspace, isPending: isRemovingWorkspace } =
+    UseRemoveWorkspace();
+
+  const handleEdit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    updateWorkspace(
+      {
+        id: workspaceId,
+        name: value,
+      },
+      {
+        onSuccess: () => {
+          toast.success("Workspace updated");
+          setEditOpen(false);
+        },
+        onError: () => {
+          toast.success("Failed to update workspace");
+        },
+      }
+    );
+  };
+
+  const handleRemove = async () => {
+    const ok = await confirm();
+
+    if (!ok) return;
+
+    removeWorkspace(
+      {
+        id: workspaceId,
+      },
+      {
+        onSuccess: () => {
+          toast.success("Workspace removed");
+          router.replace("/");
+        },
+        onError: () => {
+          toast.success("Failed to remove workspace");
+        },
+      }
+    );
+  };
+
+  return (
+    <>
+      <ConfirmDialog />
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="overflow-hidden p-0 bg-gray-200">
+          <DialogHeader className="p-4 border-b bg-white">
+            <DialogTitle>{value}</DialogTitle>
+          </DialogHeader>
+          <div className="flex flex-col gap-y-2 px-4 pb-4">
+            <Dialog open={editOpen} onOpenChange={setEditOpen}>
+              <DialogTrigger asChild>
+                <div className="px-5 py-4 rounded-lg cursor-pointer bg-white hover:bg-gray-100 transition-all duration-200">
+                  <div className="flex items-center justify-between">
+                    <p className="text-sm font-semibold">Workspace name</p>
+                    <p className="text-sm font-semibold text-[#1264A3] hover:underline">
+                      Edit
+                    </p>
+                  </div>
+                  <p className="text-sm">{value}</p>
+                </div>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Rename this worksapce</DialogTitle>
+                </DialogHeader>
+                <form className="space-y-4" onSubmit={handleEdit}>
+                  <Input
+                    value={value}
+                    disabled={isUpdatingWorkspace}
+                    onChange={(e) => setValue(e.target.value)}
+                    placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+                    required
+                    autoFocus
+                    minLength={3}
+                    maxLength={80}
+                  />
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant="outline" disabled={isUpdatingWorkspace}>
+                        Cancel
+                      </Button>
+                    </DialogClose>
+                    <Button disabled={isUpdatingWorkspace}>Save</Button>
+                  </DialogFooter>
+                </form>
+              </DialogContent>
+            </Dialog>
+            <button
+              className="flex items-center gap-x-2 px-5 py-4 rounded-lg border cursor-pointer bg-white hover:bg-gray-100 text-rose-600 transition-all duration-200"
+              disabled={isRemovingWorkspace}
+              onClick={handleRemove}
+            >
+              <TrashIcon className="size-4" />
+              <p className="text-sm font-semibold">Delete workspace</p>
+            </button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -8,6 +8,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Hint } from "@/components/hint";
+import { PreferencesModal } from "./preferences-modal";
 import { Doc } from "../../../../convex/_generated/dataModel";
 import { ChevronDown, ListFilter, SquarePen } from "lucide-react";
 
@@ -21,61 +22,72 @@ export const WorkspaceHeader = ({
   isAdmin,
 }: WorkspaceHeaderProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [preferencesOpen, setPreferencesOpen] = useState(false);
 
   return (
-    <div className="flex items-center justify-between px-4 h-[50px] gap-0.5">
-      <DropdownMenu onOpenChange={(open) => setIsOpen(open)}>
-        <DropdownMenuTrigger asChild>
-          <Button
-            size="sm"
-            variant="transparent"
-            className="w-auto overflow-hidden text-lg font-semibold p-1.5"
-          >
-            <span className="truncate">{workspace.name}</span>
-            <ChevronDown
-              className={`size-5 shrink-0 ml-1 transition-transform ${
-                isOpen ? "rotate-180" : ""
-              }`}
-            />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent side="bottom" align="start" className="w-56">
-          <DropdownMenuItem className="capitalize w-56">
-            <div className="flex items-center justify-center mr-2 relative overflow-hidden bg-[#616061] text-white font-semibold text-xl shrink-0 rounded-md size-9">
-              {workspace.name.charAt(0).toUpperCase()}
-            </div>
-            <div className="flex flex-col items-start overflow-hidden w-full">
-              <p className="font-bold truncate">{workspace.name}</p>
-              <p className="text-xs to-muted-foreground whitespace-nowrap">
-                Active Workspace
-              </p>
-            </div>
-          </DropdownMenuItem>
-          {isAdmin && (
-            <>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem className="py-2" onClick={() => {}}>
-                Invite people to {workspace.name}
-              </DropdownMenuItem>
-              <DropdownMenuItem className="py-2" onClick={() => {}}>
-                Perferences
-              </DropdownMenuItem>
-            </>
-          )}
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <div className="flex items-center gap-1">
-        <Hint label="Filter conversations" side="bottom">
-          <Button variant="transparent" size="iconSm">
-            <ListFilter className="size-5" />
-          </Button>
-        </Hint>
-        <Hint label="New message" side="bottom">
-          <Button variant="transparent" size="iconSm">
-            <SquarePen className="size-5" />
-          </Button>
-        </Hint>
+    <>
+      <PreferencesModal
+        open={preferencesOpen}
+        setOpen={setPreferencesOpen}
+        initialValue={workspace.name}
+      />
+      <div className="flex items-center justify-between px-4 h-[50px] gap-0.5">
+        <DropdownMenu onOpenChange={(open) => setIsOpen(open)}>
+          <DropdownMenuTrigger asChild>
+            <Button
+              size="sm"
+              variant="transparent"
+              className="w-auto overflow-hidden text-lg font-semibold p-1.5"
+            >
+              <span className="truncate">{workspace.name}</span>
+              <ChevronDown
+                className={`size-5 shrink-0 ml-1 transition-transform ${
+                  isOpen ? "rotate-180" : ""
+                }`}
+              />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent side="bottom" align="start" className="w-56">
+            <DropdownMenuItem className="capitalize w-56">
+              <div className="flex items-center justify-center mr-2 relative overflow-hidden bg-[#616061] text-white font-semibold text-xl shrink-0 rounded-md size-9">
+                {workspace.name.charAt(0).toUpperCase()}
+              </div>
+              <div className="flex flex-col items-start overflow-hidden w-full">
+                <p className="font-bold truncate">{workspace.name}</p>
+                <p className="text-xs to-muted-foreground whitespace-nowrap">
+                  Active Workspace
+                </p>
+              </div>
+            </DropdownMenuItem>
+            {isAdmin && (
+              <>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="py-2" onClick={() => {}}>
+                  Invite people to {workspace.name}
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className="py-2"
+                  onClick={() => setPreferencesOpen(true)}
+                >
+                  Perferences
+                </DropdownMenuItem>
+              </>
+            )}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <div className="flex items-center gap-1">
+          <Hint label="Filter conversations" side="bottom">
+            <Button variant="transparent" size="iconSm">
+              <ListFilter className="size-5" />
+            </Button>
+          </Hint>
+          <Hint label="New message" side="bottom">
+            <Button variant="transparent" size="iconSm">
+              <SquarePen className="size-5" />
+            </Button>
+          </Hint>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
