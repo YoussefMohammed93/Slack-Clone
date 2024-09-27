090d81443df2faae368cab25ea134d62016f60a2
Youssef Mohammed <youssefmohammed2093@gmail.com>
2024-09-27 18:52:12 +0300

    Editor submit feature

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,6 +1,10 @@
 import Quill from "quill";
-import { useRef } from "react";
+import { toast } from "sonner";
 import dynamic from "next/dynamic";
+import { useRef, useState } from "react";
+import { useChannelId } from "@/hooks/use-channel-id";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { UseCreateMessage } from "@/features/messages/api/use-create-message";
 
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
@@ -9,15 +13,50 @@ interface ChatInputProps {
 }
 
 export const ChatInput = ({ placeholder }: ChatInputProps) => {
+  const channelId = useChannelId();
+  const workspaceId = useWorkspaceId();
   const editorRef = useRef<Quill | null>(null);
+
+  const [editorKey, setEditorKey] = useState(0);
+  const [isPending, setIsPending] = useState(false);
+
+  const { mutate: createMessage } = UseCreateMessage();
+
+  const handelSubmit = async ({
+    body,
+    // image, // TODO : Image upload feature
+  }: {
+    body: string;
+    image: File | null;
+  }) => {
+    try {
+      setIsPending(true);
+
+      await createMessage(
+        {
+          body,
+          channelId,
+          workspaceId,
+        },
+        { throwErr: true }
+      );
+
+      setEditorKey((prevKey) => prevKey + 1);
+    } catch (err) {
+      toast.error("Failed to send message!");
+    } finally {
+      setIsPending(false);
+    }
+  };
 
   return (
     <div className="w-full px-4">
       <Editor
+        key={editorKey}
         variant="create"
         placeholder={placeholder}
-        onSubmit={() => {}}
-        disabled={false}
+        onSubmit={handelSubmit}
+        disabled={isPending}
         innerRef={editorRef}
       />
     </div>
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -38,8 +38,8 @@ interface Emoji {
 }
 
 const Editor = ({
+  onCancel,
   onSubmit,
-  // onCancel,
   placeholder = "Write something...",
   defaultValue = [],
   disabled = false,
@@ -83,8 +83,17 @@ const Editor = ({
             enter: {
               key: "Enter",
               handler: () => {
-                // TODO : Submit form
-                return;
+                const text = quill.getText();
+                const addedImage = imageElementRef.current?.files?.[0] || null;
+                const isEmpty =
+                  !addedImage &&
+                  text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+
+                if (isEmpty) return;
+
+                const body = JSON.stringify(quill.getContents());
+
+                submitRef.current?.({ body, image: addedImage });
               },
             },
             shift_enter: {
@@ -146,7 +155,7 @@ const Editor = ({
     quill?.insertText(quill?.getSelection()?.index || 0, emoji.native);
   };
 
-  const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+  const isEmpty = !image && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
   return (
     <div className="flex flex-col">
@@ -157,7 +166,12 @@ const Editor = ({
         ref={imageElementRef}
         onChange={(event) => setImage(event.target.files![0])}
       />
-      <div className="flex flex-col border border-slate-200 focus-within:border-slate-300 focus-within:shadow-sm rounded-lg transition-all duration-150 bg-white overflow-hidden">
+      <div
+        className={cn(
+          "flex flex-col border border-slate-200 focus-within:border-slate-300 focus-within:shadow-sm rounded-lg transition-all duration-150 bg-white overflow-hidden",
+          disabled && "opacity-50"
+        )}
+      >
         <div ref={containerRef} className="h-full ql-custom" />
         {!!image && (
           <div className="p-2">
@@ -213,14 +227,19 @@ const Editor = ({
               <Button
                 variant="outline"
                 size="sm"
-                onClick={() => {}}
+                onClick={onCancel}
                 disabled={disabled}
               >
                 Cancel
               </Button>
               <Button
                 size="sm"
-                onClick={() => {}}
+                onClick={() => {
+                  onSubmit({
+                    body: JSON.stringify(quillRef.current?.getContents()),
+                    image,
+                  });
+                }}
                 disabled={disabled || isEmpty}
                 className="bg-emerald-600 hover:bg-emerald-600/85 text-white transition-all duration-150"
               >
@@ -231,7 +250,12 @@ const Editor = ({
           {variant === "create" && (
             <Button
               disabled={disabled || isEmpty}
-              onClick={() => {}}
+              onClick={() => {
+                onSubmit({
+                  body: JSON.stringify(quillRef.current?.getContents()),
+                  image,
+                });
+              }}
               size="iconSm"
               className={cn(
                 "ml-auto",
